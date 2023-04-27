ace8f7f26b1c447d20c58a420a7b6dfa6d26e445
Benjamin A <97291322+Baoufa@users.noreply.github.com>
2023-04-27 15:03:15 +0200

    feat: add imports

--- space-config/sismo-contributors/index.ts
+++ space-config/sismo-contributors/index.ts
@@ -1,5 +1,7 @@
 // add an images folder in your space folder if you would like Sismo to host your images
 
+import { SpaceConfig } from "../space-config.types";
+
 export const sismoContributorsConfig : SpaceConfig = {
   slug: 'sismo-contributors',
   name: 'Sismo Contributors',

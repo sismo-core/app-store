44c4a3e6401bec2d32aa09f5e26925ede3aa10a8
Dev <gabinhry@gmail.com>
2023-05-03 09:57:36 +0200

    fix: return application/json

--- src/app/api/spaces/[slug]/route.ts
+++ src/app/api/spaces/[slug]/route.ts
@@ -2,12 +2,15 @@ import * as config from "@/space-config";
 
 export async function GET(request: Request, { params }: { params: { slug: string } }) {
 
-  console.log("GET space config", params.slug, config);
-
   const selectedSpace = Object.values(config).find((space) => {
     return space.slug === params.slug;
   });
 
-  return new Response(JSON.stringify(selectedSpace));
+  return new Response(JSON.stringify(selectedSpace), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
 }
 
--- src/libs/getBaseUrl.ts
+++ src/libs/getBaseUrl.ts
@@ -2,6 +2,6 @@ import { cache } from 'react';
 
 export const getBaseUrl = cache(() =>
   process.env.VERCEL_URL
-    ? `https://sismo-spaces.vercel.app`
-    : `https://sismo-spaces.vercel.app`//`http://localhost:${process.env.PORT ?? 3000}`,
+    ? `https://${process.env.VERCEL_URL}`
+    : `http://localhost:${process.env.PORT ?? 3000}`,
 );

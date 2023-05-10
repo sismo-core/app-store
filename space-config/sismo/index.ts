61f64760956085ec073a3d7cd33754200d081d9e
Quentin Finck <quentin@sismo.io>
2023-05-10 15:26:40 +0200

    feat: add put app id & images

---
+++ space-config/sismo/index.ts
@@ -0,0 +1,11 @@
+// add an images folder in your space folder if you would like Sismo to host your images
+
+import { SpaceConfig } from "../types";
+
+export const sismoConfig: SpaceConfig = {
+  slug: 'sismo',
+  name: 'Sismo',
+  description:
+    "Sismo enables users to selectively disclose personal data to apps with Sismo Connect (SSO) and ZK Badges (SBTs). Get the most of your data",
+  coverImage: "banner.png"
+};
--- space-config/the-merge-contributors/index.ts
+++ space-config/the-merge-contributors/index.ts
@@ -5,7 +5,7 @@ import { SpaceConfig } from "../types";
 export const theMergeContributorsConfig: SpaceConfig = {
   slug: "the-merge-contributors",
   name: "The Merge Contributors",
-  description: "Tribute to contributors to the Ethereum merge.",
+  description: "Tribute to contributors to the Ethereum Merge.",
   profileImage: "pfp_Space_TheMergeContributors_400x400.png",
   coverImage: "Cover_Space_TheMergeContributors_1160x340.png",
   demoEnabled: true,
@@ -14,9 +14,9 @@ export const theMergeContributorsConfig: SpaceConfig = {
     {
       type: "external",
       name: "Mergooor Pass",
-      description: "Claim a Mergooor Pass NFT - open to contributors to The Merge.",
+      description: "Claim your Mergooor Pass NFT - open to contributors to The Merge.",
       tags: ["NFT"],
-      image: "pfp_Space_TheMergeContributors_400x400.png",
+      image: "Mergooor NFT_1014x720px.png",
       CTAText: "Get NFT",
       link: "https://claim.zkdrop.io/mergooor-pass",
 
@@ -27,7 +27,7 @@ export const theMergeContributorsConfig: SpaceConfig = {
       name: "EthCC ticket exclusive access",
       description: "Register your email address to receive exclusive tickets for web3 events - open to contributors to The Merge.",
       tags: ["Event"],
-      image: "pfp_Space_TheMergeContributors_400x400.png",
+      image: "EthCCtickets_1014x720px.png",
       CTAText: "Register to get tickets",
       fields: [
         {
@@ -51,8 +51,8 @@ export const theMergeContributorsConfig: SpaceConfig = {
         description: "You have successfully registered to receive exclusive tickets for web3 events.",
       },
       output: "google_sheet",
-      appId: null,
-      spreadsheetId: null,
+      appId: "0x6de7c6f801c960f78168f67de13396cb",
+      spreadsheetId: "1y94lpA5kWbGi_u50C-GlYKpeJp8aYxhI37D2GDVg",
       claimRequests: [{ groupId: "0x42c768bb8ae79e4c5c05d3b51a4ec74a" }]
     },
   ],

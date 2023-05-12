3abd4d800134f90dd803d339e70423bc9c8f5185
gabin54 <59560717+gabin54@users.noreply.github.com>
2023-05-12 11:12:39 +0200

    fix: add env REDIRECTION for the home redirection (#18)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -10,14 +11,14 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'src')],
  },
  async redirects() {
-  return [
-    {
-      source: '/',
-      destination: '/the-merge-contributors',
-      permanent: true,
-    },
-  ];
-},
+    return [
+      {
+        source: '/',
+        destination: process.env.REDIRECTION,
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig

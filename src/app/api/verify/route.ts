7edf7a202a082d9359e1ff9a236e8b79c6041c62
Dev <gabinhry@gmail.com>
2023-05-12 15:13:38 +0200

    fix: upgrade vercel capacity

--- src/app/api/verify/route.ts
+++ src/app/api/verify/route.ts
@@ -20,19 +20,19 @@ export async function POST(req: Request) {
     if (!env.isDemo) {
         const result = await verifyResponse(app, response);
         if (!result) return new Response(null, { status: 500, statusText: "Invalid response" });
-        // if (needVaultAuth(app)) {
-        //     const vaultId = await result.getUserId(AuthType.VAULT);
-        //     if (!vaultId) return new Response(null, { status: 500, statusText: "No Vault Id" });
-        //     fieldsToAdd = [
-        //         ...fieldsToAdd,
-        //         {
-        //             name: "VaultId",
-        //             value: vaultId
-        //         }
-        //     ];
-        //     const isExist = await isVaultIdExist(store, vaultId);
-        //     if (isExist) return NextResponse.json({ status: "already-subscribed" })
-        // } 
+        if (needVaultAuth(app)) {
+            const vaultId = await result.getUserId(AuthType.VAULT);
+            if (!vaultId) return new Response(null, { status: 500, statusText: "No Vault Id" });
+            fieldsToAdd = [
+                ...fieldsToAdd,
+                {
+                    name: "VaultId",
+                    value: vaultId
+                }
+            ];
+            const isExist = await isVaultIdExist(store, vaultId);
+            if (isExist) return NextResponse.json({ status: "already-subscribed" })
+        } 
     } else {
         if (needVaultAuth(app)) {
             fieldsToAdd = [

4ee58469c6672cb8ea8c3a0a7fcbf2c0463a051b
Dev <gabinhry@gmail.com>
2023-05-12 19:43:39 +0200

    fix: remove buffer utils

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   sassOptions: {
     additionalData: `@use "styles/color" as color; @use "styles/mixin" as mixin;`,
     includePaths: [path.join(__dirname, 'src')],

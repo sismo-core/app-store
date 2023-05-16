d0a8f8e383b37b1d3a713adb98e28607701d418d
Dev <gabinhry@gmail.com>
2023-05-16 13:20:55 +0200

    fix: add landing

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,14 +2,31 @@
 import Hero from "@/src/components/Hero";
 import Spaces from "@/src/components/Spaces";
 import { getSpacesConfigs } from "../../libs/spaces/getSpaces";
+import getImgSrcFromConfig from "@/src/utils/getImgSrcFromConfig";
+import { SpaceConfig } from "@/space-config/types";
+
+export type SpaceImportedImage = {
+  config: SpaceConfig,
+  link: string
+}
 
 export default async function LandingPage() {
   const configs = await getSpacesConfigs();
 
-  return (
-    <main>
+  let spaceImportedImages: SpaceImportedImage[] = [];
+  await Promise.all(configs.map(async config => spaceImportedImages.push({
+    config: config,
+      link: await getImgSrcFromConfig(
+        config?.slug,
+        config?.profileImage
+      )
+  })));
+
+  console.log("spaceImportedImages", spaceImportedImages);
+
+  return (<>
       <Hero />
-      <Spaces configs={configs}/>
-    </main>
+      <Spaces configs={configs} spaceImportedImages={spaceImportedImages}/>
+      </>
   );
 }

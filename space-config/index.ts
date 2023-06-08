c8407146b07c81b9bb91f6c64a97d7d170edc8b7
Quentin Finck <quentin@sismo.io>
2023-06-08 17:31:06 +0200

    feat: add new config demo sismoapps-tutorials

--- space-config/index.ts
+++ space-config/index.ts
@@ -2,9 +2,11 @@ import { SpaceConfig } from "./types";
 import { theMergeContributorsConfig } from "./the-merge-contributors";
 import { sismoConfig } from "./sismo";
 import { privacyIsNormalConfig } from "./privacy-is-normal";
+import { sismoAppsTutorialsConfig } from "./sismoapps-tutorials";
 
 export const configs: SpaceConfig[] = [
     theMergeContributorsConfig,
     sismoConfig,
-    privacyIsNormalConfig
+    privacyIsNormalConfig,
+    sismoAppsTutorialsConfig
 ]

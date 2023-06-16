28c3eb1ea00cdba5010f97d12d060694dd96abce
Quentin Finck <quentin@sismo.io>
2023-06-16 15:36:22 +0200

    feat: remove sismoappstuto

--- space-config/index.ts
+++ space-config/index.ts
@@ -2,11 +2,9 @@ import { SpaceConfig } from "./types";
 import { theMergeContributorsConfig } from "./the-merge-contributors";
 import { sismoConfig } from "./sismo";
 import { privacyIsNormalConfig } from "./privacy-is-normal";
-import { sismoAppsTutorialsConfig } from "./sismoapps-tutorials";
 
 export const configs: SpaceConfig[] = [
     theMergeContributorsConfig,
     sismoConfig,
-    privacyIsNormalConfig,
-    sismoAppsTutorialsConfig
+    privacyIsNormalConfig
 ]

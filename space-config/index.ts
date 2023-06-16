dea4ae1f8a80f35a1593fe7005c2aebfa8c74a44
ZKentin <126152532+ZKentin@users.noreply.github.com>
2023-06-16 17:37:09 +0200

    Aave chan initiative (#68)
    * feat: add new config demo sismoapps-tutorials
    
    * feat: add new config aave-chan-initiative
    
    * feat: update groupid
    
    * feat: update wording
    
    * feat: update x img
    
    * feat: update cover
    
    * feat: update appdescription
    
    * feat: fix wording delegators
    
    * aci
    
    * aci save auth & claim
    
    * feat: add impersonation config

--- space-config/index.ts
+++ space-config/index.ts
@@ -2,9 +2,11 @@ import { SpaceConfig } from "./types";
 import { theMergeContributorsConfig } from "./the-merge-contributors";
 import { sismoConfig } from "./sismo";
 import { privacyIsNormalConfig } from "./privacy-is-normal";
+import { aaveChanInitiativeConfig } from "./aave-chan-initiative";
 
 export const configs: SpaceConfig[] = [
     theMergeContributorsConfig,
     sismoConfig,
     privacyIsNormalConfig,
+    aaveChanInitiativeConfig
 ]

6b8d9b3dad87b274e6d5522f24b99bc7cbfdab8e
Quentin Finck <quentin@sismo.io>
2023-06-22 13:01:51 -0400

    update wording

--- space-config/synaps/index.ts
+++ space-config/synaps/index.ts
@@ -13,11 +13,11 @@ export const synapsConfig: SpaceConfig = {
   apps: [
     {
       type: "external",
-      name: "Synaps Liveness",
-      description: "Synaps provides identity verification solutions from Personhood validation to regulated KYC / AML and KYB processes",
-      tags: ["KYC"],
+      name: "Liveness Verification",
+      description: "Perform liveness verification with Synaps to join the 'Proof of Liveness' Data Group to access Sismo Apps that request to be part of it. Through this liveness session, a privacy-preserving encrypted vector graph is generated. It contains no biometric data and cannot be used to recreate your face.",
+      tags: ["Liveness"],
       image: "synaps_liveness_img.png",
-      CTAText: "Prove liveness",
+      CTAText: "Prove your Liveness",
       slug: "proof-of-liveness",
       envs: ["Demo", "Prod"],
       authRequests: [{ authType: AuthType.VAULT }],

70d9825b2701c133d3d7aaeb78697680914a6ec4
Dimitri <69167058+dimsome@users.noreply.github.com>
2023-07-03 15:04:36 +0700

    feat: fof prod config

---
+++ space-config/sismo/main.ts
@@ -0,0 +1,103 @@
+import { initAppDataSource } from "../../src/libs/user-store/postgres-user-store/initAppDataSource";
+// add an images folder in your space folder if you would like Sismo to host your images
+import { AuthType } from "@sismo-core/sismo-connect-server";
+import { SpaceConfig } from "../types";
+
+export const sismoConfigMain: SpaceConfig = {
+  metadata: {
+    slug: "sismo",
+    name: "Sismo",
+    description:
+      "This Space showcases a variety of Sismo Apps and benefits for our valued members across all levels (1, 2, and 3) in the Sismo Community. Prove your membership and unlock access to Apps personalized to your level. To learn more and elevate your level, head to https://community.sismo.io.",
+    profileImage: "space_sismo_pfp_400x400.png",
+    coverImage: "space_sismo_cover_1740x540.png",
+    socialLinks: [
+      {
+        type: "link",
+        link: "https://www.sismo.io/",
+      },
+      {
+        type: "twitter",
+        link: "https://twitter.com/Sismo_eth",
+      },
+      {
+        type: "discord",
+        link: "https://discord.com/invite/sismo",
+      },
+      {
+        type: "github",
+        link: "https://github.com/sismo-core",
+      },
+    ],
+  },
+  apps: [
+    {
+      type: "zkForm",
+      metadata: {
+        name: "Future of France Invitation",
+        slug: "ticket-fof",
+        description:
+          "Register to get a chance to receive a ticket for the Future of France Party. The event will take place on July 17th during the EthCC week. Exclusively for members of Sismo Community Level 3.",
+        tags: ["Event", "Ticket"],
+        image: "sismo_appstore_fof_tickets.png",
+        ctaText: "Register to get your Ticket",
+      },
+      sismoConnectRequest: {
+        appId: "0x08ffa7336eb7bc0907a9f76ebc55aa4d",
+        authRequests: [{ authType: AuthType.VAULT }],
+        claimRequests: [{ groupId: "0xd630aa769278cacde879c5c0fe5d203c", value: 3 }],
+        impersonateAddresses: ["0x8ab1760889F26cBbf33A75FD2cF1696BFccDc9e6"],
+      },
+      templateConfig: {
+        fields: [
+          {
+            type: "short-text",
+            label: "First Name",
+            isRequired: true,
+          },
+          {
+            type: "short-text",
+            label: "Last Name",
+            isRequired: true,
+          },
+          {
+            type: "short-text",
+            label: "Company (optional)",
+            isRequired: false,
+          },
+          {
+            type: "short-text",
+            label: "Email",
+            isRequired: true,
+          },
+          {
+            type: "short-text",
+            label: "Will you be in Paris on July 17, 7pm? Tickets are limited.",
+            isRequired: true,
+            placeholder: "Yes/No",
+          },
+        ],
+        congratulationsMessage: {
+          title: "Congratulations!",
+          description: "You will receive the ticket in a few days! See you there 💜",
+        },
+        failedMessage: {
+          title: "Sorry, too late this time 😕",
+          description: "Stay in touch for the next one 💜",
+        },
+        output: {
+          destination: {
+            type: "google_sheet",
+            spreadsheetId: "16NuT1HIJvM2jikRmHzN-ZSwJmjg_Ef1-6e-XbPff3z4",
+          },
+          saveAuths: true,
+          saveClaims: true,
+        },
+        userSelection: { type: "FCFS", maxNumberOfUsers: 40 },
+      },
+      options: {
+        endDate: new Date("2023-07-10T18:00"),
+      },
+    },
+  ],
+};

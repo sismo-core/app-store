b968b7a3695091402ff7c3255dba26750d2f135e
Dimitri <69167058+dimsome@users.noreply.github.com>
2023-07-03 14:57:26 +0700

    feat: zkTelegram bot to prod

---
+++ space-config/sismo/main.ts
@@ -0,0 +1,56 @@
+import { initAppDataSource } from "../../src/libs/user-store/postgres-user-store/initAppDataSource";
+// add an images folder in your space folder if you would like Sismo to host your images
+import { AuthType } from "@sismo-core/sismo-connect-server";
+import { SpaceConfig } from "../types";
+
+export const sismoConfigMain: SpaceConfig = {
+  metadata: {
+    slug: "sismo",
+    name: "Sismo",
+    description:
+      "Sismo enables users to selectively disclose personal data to apps with Sismo Connect (SSO) and ZK Badges (SBTs). Get the most of your data.",
+    profileImage: "space_sismo_pfp_400x400.png",
+    coverImage: "space_sismo_cover_1740x540.png",
+    socialLinks: [
+      {
+        type: "link",
+        link: "https://www.sismo.io/",
+      },
+      {
+        type: "twitter",
+        link: "https://twitter.com/Sismo_eth",
+      },
+      {
+        type: "discord",
+        link: "https://discord.com/invite/sismo",
+      },
+      {
+        type: "github",
+        link: "https://github.com/sismo-core",
+      },
+    ],
+  },
+  apps: [
+    {
+      type: "zkTelegramBot",
+      metadata: {
+        name: "Citadel Invite",
+        slug: "telegram",
+        description:
+          "Sismo Citadel telegram chat was created specifically for our most active builders and closest friends. It is gated to Sismo Community level 3. Learn more about community levels: https://community.sismo.io.",
+        image: "sismoapps_tg_zk_bot_1014x720.png",
+        tags: ["Telegram"],
+        ctaText: "Join Gated Telegram Chat",
+      },
+      sismoConnectRequest: {
+        appId: "0xa83b7b0d5e268fb4aa3d3582e11700fa",
+        claimRequests: [{ groupId: "0xd630aa769278cacde879c5c0fe5d203c", value: 3 }],
+        impersonateAddresses: ["telegram:dhadrien:1234", "dhadrien.eth"],
+      },
+      templateConfig: {
+        telegramGroupId: "-1001930531830",
+        telegramInviteLink: "https://t.me/+3hZN4bNCmsw5Mzgy",
+      },
+    },
+  ],
+};

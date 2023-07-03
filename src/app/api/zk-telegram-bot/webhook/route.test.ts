e83066b9b58c0483de221d10bb1292bfbe47325f
Alexandru Pele <pelealexandru@gmail.com>
2023-07-03 16:17:23 +0200

    feat: add groupid command handler (#101)

--- src/app/api/zk-telegram-bot/webhook/route.test.ts
+++ src/app/api/zk-telegram-bot/webhook/route.test.ts
@@ -7,7 +7,7 @@ import axios from "axios";
 import { getUserStore } from "@/src/libs/user-store";
 import { UserStore } from "@/src/libs/user-store/store";
 import { MemoryUserStore } from "@/src/libs/user-store/memory-user-store";
-import { MockedRequest, mockJoinRequest, mockSpacesType } from "../mocks";
+import { MockedRequest, mockGroupIdCommand, mockJoinRequest, mockSpacesType } from "../mocks";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -34,7 +34,7 @@ describe("POST /api/zk-telegram-bot/webhook", () => {
     jest.resetModules();
     memoryUserStore = new MemoryUserStore();
     (getUserStore as jest.Mock).mockReturnValue(memoryUserStore);
-    (getSpaces as jest.Mock).mockReturnValue(mockSpacesType());
+    (getSpaces as jest.Mock).mockReturnValue(mockSpacesType("appSlug", "spaceSlug", "-2"));
   });
 
   it("Should return error when app is not found", async () => {
@@ -86,5 +86,24 @@ describe("POST /api/zk-telegram-bot/webhook", () => {
     const data = await response.json();
     expect(performedDeclineRequest).toEqual(true);
     expect(data.status).toEqual("declined");
+  });
+
+  it("Should reply to chat_id command", async () => {
+    (getSpaces as jest.Mock).mockReturnValue(mockSpacesType("appSlug", "spaceSlug", "-2"));
+
+    let performedSendMessage = false;
+    mockedAxios.get.mockImplementation((url) => {
+      if (url === "https://api.telegram.org/bot123/sendMessage?chat_id=-2&reply_to_message_id=1&text=-2") {
+        performedSendMessage = true;
+      }
+      return Promise.resolve();
+     });
+
+    const response = await POST(
+      new MockedRequest(mockGroupIdCommand(-2, 1)) as any
+    );
+    const data = await response.json();
+    expect(performedSendMessage).toEqual(true);
+    expect(data.status).toEqual("ok");
   });
 });

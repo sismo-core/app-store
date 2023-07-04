2f07382e96b8c6c56c5c6c4c5110ab0b0026e0fc
Dimitri <69167058+dimsome@users.noreply.github.com>
2023-07-04 21:07:23 +0700

    fix: remove unused imports

--- space-config/index.ts
+++ space-config/index.ts
@@ -8,14 +8,12 @@ import { theMergeContributorsConfigDemo } from "@/space-config/the-merge-contrib
 import { sismoConfigDemo } from "@/space-config/sismo/demo";
 import { privacyIsNormalConfigDemo } from "@/space-config/privacy-is-normal/demo";
 import { aaveChanInitiativeConfigDemo } from "@/space-config/aave-chan-initiative/demo";
-import { testConfigMain } from "@/space-config/test/main";
 
 export const configsMain: SpaceConfig[] = [
   theMergeContributorsConfigMain,
   privacyIsNormalConfigMain,
   aaveChanInitiativeConfigMain,
   sismoConfigMain,
-  testConfigMain,
 ];
 
 export const configsDemo: SpaceConfig[] = [

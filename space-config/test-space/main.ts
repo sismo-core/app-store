eba06e03f6f2d905674fb73e22417606291b5e92
kugusha <kugush7@gmail.com>
2023-07-04 10:12:06 +0200

    set up my configs

--- space-config/test-space/main.ts
+++ space-config/test-space/main.ts
@@ -5,27 +5,15 @@ import { SpaceConfig } from "../types";
 //           UPDATE HERE ↓↓↓
 export const myNewSpaceConfigMain : SpaceConfig = {
   metadata: {
-    slug: "aaa-my-new-space", // UPDATE HERE
-    name: "My New Space", // UPDATE HERE
+    slug: "test-space", // UPDATE HERE
+    name: "Look at my space", // UPDATE HERE
     description:
       "This Space is dedicated to my new project. It's a great project, you should check it out!", // UPDATE HERE
     profileImage: "my_new_space_picture_400x400.png", // UPDATE HERE
     socialLinks: [
       {
         type: "link",
-        link: "https://www.sismo.io/", // UPDATE HERE
-      },
-      {
-        type: "twitter",
-        link: "https://twitter.com/Sismo_eth", // UPDATE HERE
-      },
-      {
-        type: "discord",
-        link: "https://discord.com/invite/sismo", // UPDATE HERE
-      },
-      {
-        type: "github",
-        link: "https://github.com/sismo-core", // UPDATE HERE
+        link: "https://etherscan.io/tx/0x0ae3d3ce3630b5162484db5f3bdfacdfba33724ffb195ea92a6056beaa169490", // UPDATE HERE
       },
     ],
   }, 

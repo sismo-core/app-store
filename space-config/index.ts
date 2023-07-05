132adff13f3884a45a77afa23fbd37916ca07a82
ZKentin <126152532+ZKentin@users.noreply.github.com>
2023-07-05 18:54:50 +0200

    Adding Cow Swap Space (#141)
    * add space config cow-swap
    
    * add app config cow-swag
    
    * add app config cow-event

--- space-config/index.ts
+++ space-config/index.ts
@@ -3,16 +3,19 @@ import { theMergeContributorsConfigMain } from "@/space-config/the-merge-contrib
 import { privacyIsNormalConfigMain } from "@/space-config/privacy-is-normal/main";
 import { aaveChanInitiativeConfigMain } from "./aave-chan-initiative/main";
 import { sismoConfigMain } from "@/space-config/sismo/main";
+import { cowSwapConfigMain } from "@/space-config/cow-swap/main";
 import { theMergeContributorsConfigDemo } from "@/space-config/the-merge-contributors/demo";
 import { sismoConfigDemo } from "@/space-config/sismo/demo";
 import { privacyIsNormalConfigDemo } from "@/space-config/privacy-is-normal/demo";
 import { aaveChanInitiativeConfigDemo } from "@/space-config/aave-chan-initiative/demo";
+import { cowSwapConfigDemo } from "@/space-config/cow-swap/demo";
 
 export const configsMain: SpaceConfig[] = [
   theMergeContributorsConfigMain,
   privacyIsNormalConfigMain,
   aaveChanInitiativeConfigMain,
   sismoConfigMain,
+  cowSwapConfigMain,
 ];
 
 export const configsDemo: SpaceConfig[] = [
@@ -20,4 +23,5 @@ export const configsDemo: SpaceConfig[] = [
   sismoConfigDemo,
   privacyIsNormalConfigDemo,
   aaveChanInitiativeConfigDemo,
+  cowSwapConfigDemo,
 ];

2ce732ee5bc47070a591440deb8c3c588925f48c
leo21.sismo.eth <leo@sismo.io>
2023-07-09 19:47:07 +0200

    feat: remove typeorm errors in local (#177)

--- next.config.js
+++ next.config.js
@@ -4,9 +4,8 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@sismo-core/sismo-connect-server"],
+    serverComponentsExternalPackages: ["@sismo-core/sismo-connect-server", "typeorm"],
   },
-
   sassOptions: {
     additionalData: `@use "styles/color" as color; @use "styles/mixin" as mixin;`,
     includePaths: [path.join(__dirname, 'src')],

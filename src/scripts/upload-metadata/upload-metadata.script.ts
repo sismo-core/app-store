a8d94980a32676f65b00553358773e1265b3fd8a
leo Sayous <leo@sismo.io>
2023-07-10 16:54:27 +0200

    feat: test ci

--- src/scripts/upload-metadata/upload-metadata.script.ts
+++ src/scripts/upload-metadata/upload-metadata.script.ts
@@ -1,5 +1,17 @@
+import { Command } from "commander";
 import { isEnv } from "@/src/environments";
 import { uploadMetadata } from "./upload-metadata";
 
+const program = new Command();
 
-uploadMetadata(["sismo"], isEnv("demo") ? "demo" : "main");
+program
+  .description("Upload zk drop template metadata to ipfs")
+  .argument("<env>", "string to split")
+  .argument("<spaces...>", "string to split")
+  .action((env, spaces) => {
+    console.log("env", env);
+    console.log("spaces", spaces);
+    uploadMetadata(spaces, env);
+  });
+
+program.parse();

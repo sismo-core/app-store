c3954f372c1349d172661f15ea67e201a9368ed4
leo21.sismo.eth <leo@sismo.io>
2023-07-10 19:28:04 +0200

    feat: add CI for sending NFT metadata to ipfs

--- src/scripts/upload-metadata/upload-metadata.script.ts
+++ src/scripts/upload-metadata/upload-metadata.script.ts
@@ -1,5 +1,18 @@
-import { isEnv } from "@/src/environments";
+import { Command } from "commander";
 import { uploadMetadata } from "./upload-metadata";
 
+const program = new Command();
 
-uploadMetadata(["sismo"], isEnv("demo") ? "demo" : "main");
+program
+  .description("Upload zk drop template metadata to ipfs")
+  .argument("<spaces...>", "string to split")
+  .action(async (spaces) => {
+    let formattedSpaces = [];
+    for (const space of spaces) {
+      formattedSpaces.push(...space.split(" "));
+    }
+    const res = await uploadMetadata(formattedSpaces);
+    console.log(res);
+  });
+
+program.parse();
--- src/scripts/upload-metadata/upload-metadata.ts
+++ src/scripts/upload-metadata/upload-metadata.ts
@@ -1,16 +1,16 @@
 import { ZkAppType } from "@/src/services/spaces-service";
 import ServiceFactory from "@/src/services/service-factory/service-factory";
 import { pinFileToIPFS, pinJSONToIPFS } from "./utils/pin-ipfs";
+import { StdoutLogger } from "@/src/services/logger-service/stdout-logger-service";
 
-export const uploadMetadata = async (spacesSlugs: string[], env: "demo" | "main" = "main") => {
-  const sismoFactory = ServiceFactory.getSismoFactoryService();
-  const loggerService = ServiceFactory.getLoggerService();
+export const uploadMetadata = async (spacesSlugs: string[]) => {
+  const loggerService = new StdoutLogger();
   const spacesService = ServiceFactory.getSpacesService();
 
   let uploadedHash: {
     [spaceSlug: string]: {
-      [appSlug: string]: string
-    }
+      [appSlug: string]: string;
+    };
   } = {};
 
   for (const space of await spacesService.getSpaces()) {
@@ -18,9 +18,8 @@ export const uploadMetadata = async (spacesSlugs: string[], env: "demo" | "main"
     const apps: ZkAppType[] = space.apps;
     for (const app of apps) {
       if (app.type !== "zkDrop") continue;
-
       // Upload image on IPFS
-      const imagePath = `/images/${app.nftMetadata.image}`;
+      const imagePath = `${__dirname}/../../../space-configs/images/${app.nftMetadata.image}`;
       loggerService.info(`Pinning ${imagePath}...`);
       const { ipfsHash: imageIpfsHash } = await pinFileToIPFS(imagePath);
 
@@ -28,15 +27,14 @@ export const uploadMetadata = async (spacesSlugs: string[], env: "demo" | "main"
       const metadata = {
         name: app.nftMetadata.name,
         description: app.nftMetadata.description,
-        image: imageIpfsHash
-      }
-      loggerService.info(`Pinning ${metadata}...`);
+        image: `ipfs://${imageIpfsHash}`,
+      };
+      loggerService.info(`Pinning ${JSON.stringify(metadata, null, 2)}...`);
       const { ipfsHash: metadataIpfsHash } = await pinJSONToIPFS(metadata);
+      uploadedHash[space.slug] = uploadedHash[space.slug] || {};
       uploadedHash[space.slug][app.slug] = metadataIpfsHash;
     }
   }
 
   return uploadedHash;
 };
-
-

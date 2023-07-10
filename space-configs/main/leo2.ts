071912de35251263dabcd563d2d102ad63aee453
leo21.sismo.eth <leo@sismo.io>
2023-07-10 11:30:01 +0200

    Update and rename aaa-my-new-space.ts to leo2.ts (#185)
    * feat: add new space template
    
    * Update and rename aaa-my-new-space.ts to leo2.ts

---
+++ space-configs/main/leo2.ts
@@ -0,0 +1,31 @@
+// add an images folder in your space folder if you would like Sismo to host your images
+import { SpaceConfig } from "../types";
+
+//           UPDATE HERE ↓↓↓
+export default {
+  metadata: {
+    name: "leo2", // UPDATE HERE
+    description:
+      "This Space is dedicated to my new project. It's a great project, you should check it out!", // UPDATE HERE
+    image: "my_new_space_picture_400x400.png", // UPDATE HERE
+    socialLinks: [
+      {
+        type: "link",
+        link: "https://www.sismo.io/", // UPDATE HERE
+      },
+      {
+        type: "twitter",
+        link: "https://twitter.com/Sismo_eth", // UPDATE HERE
+      },
+      {
+        type: "discord",
+        link: "https://discord.com/invite/sismo", // UPDATE HERE
+      },
+      {
+        type: "github",
+        link: "https://github.com/sismo-core", // UPDATE HERE
+      },
+    ],
+  },
+  apps: [],
+} as SpaceConfig;

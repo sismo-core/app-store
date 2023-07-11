ab6a935bd4a312b8ff10783f8ca818e4872db2ab
Loïc Burger <110289209+LoicBurger@users.noreply.github.com>
2023-07-11 10:03:51 +0200

    add zkDrop template

--- space-configs/main/leo.ts
+++ space-configs/main/leo.ts
@@ -26,5 +26,38 @@ export default {
       },
     ],
   },
-  apps: [],
+  apps: [
+    {
+      type: "zkDrop",
+      metadata: {
+        name: "Leo ZK Drop", // UPDATE HERE
+        slug: "claim-your-airdrop", // UPDATE HERE
+        description: "Claim your Zk Drp", // UPDATE HERE
+        tags: ["Airdrop"],
+        image: "leo_cover_400x400.png", // UPDATE HERE
+        createdAt: new Date("2023-07-03T18:00"), // UPDATE HERE WITH CURRENT DATE
+      },
+      sismoConnectRequest: {
+        appId: "{{ auto-fill }}",
+        authRequests: [{ authType: AuthType.VAULT }],
+        claimRequests: [{ groupId: "0xd4cdaae916464870f7413a03b1fe8c31" }],  // UPDATE HERE WITH YOUR GROUP
+      },
+      templateConfig: {
+        nftMetadata: {
+          name: "NFT Claim leo test",  // UPDATE HERE
+          description: "This is a leo airdrop test",// UPDATE HERE
+          image: "leo_cover_400x400.png" // UPDATE HERE
+        },
+        chains: [{
+          contractAddress: "{{ auto-fill }}",
+          name: "sepolia", // UPDATE HERE choose your chain name
+          relayerEnabled: true, 
+          isTransferable: true
+        }],
+        step1CtaText: "Prove eligibility", 
+        step2CtaText: "Claim!",
+        appDescription: "Claim Your airdrop"
+      }
+}
+  ],
 } as SpaceConfig;

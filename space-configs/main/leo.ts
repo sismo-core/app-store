9603987a150584f0bc3a4e408a132af6489795ff
Loïc Burger <110289209+LoicBurger@users.noreply.github.com>
2023-07-11 13:54:57 +0200

    Update leo.ts - add other examples

--- space-configs/main/leo.ts
+++ space-configs/main/leo.ts
@@ -31,7 +31,7 @@ export default {
     {
       type: "zkDrop",
       metadata: {
-        name: "Leo ZK Drop", // UPDATE HERE
+        name: "Leo ZK Drop relayer", // UPDATE HERE
         slug: "claim-your-airdrop", // UPDATE HERE
         description: "Claim your Zk Drp", // UPDATE HERE
         tags: ["Airdrop"],
@@ -53,6 +53,78 @@ export default {
           contractAddress: "{{ auto-fill }}",
           name: "sepolia", // UPDATE HERE choose your chain name
           relayerEnabled: true, 
+          isTransferable: true
+        }, {
+          contractAddress: "{{ auto-fill }}",
+          name: "goerli", // UPDATE HERE choose your chain name
+          relayerEnabled: true, 
+          isTransferable: true
+        }, {
+          contractAddress: "{{ auto-fill }}",
+          name: "mumbai", // UPDATE HERE choose your chain name
+          relayerEnabled: true, 
+          isTransferable: false
+        }, {
+          contractAddress: "{{ auto-fill }}",
+          name: "gnosis", // UPDATE HERE choose your chain name
+          relayerEnabled: true, 
+          isTransferable: false
+        }, {
+          contractAddress: "{{ auto-fill }}",
+          name: "polygon", // UPDATE HERE choose your chain name
+          relayerEnabled: true, 
+          isTransferable: true
+        }],
+        step1CtaText: "Prove eligibility", 
+        step2CtaText: "Claim!",
+        appDescription: "Claim Your airdrop"
+      }
+},
+        {
+      type: "zkDrop",
+      metadata: {
+        name: "Leo ZK Drop not relayed", // UPDATE HERE
+        slug: "claim-your-airdrop", // UPDATE HERE
+        description: "Claim your Zk Drp", // UPDATE HERE
+        tags: ["Airdrop"],
+        image: "leo_cover_400x400.png", // UPDATE HERE
+        createdAt: new Date("2023-07-03T18:00"), // UPDATE HERE WITH CURRENT DATE
+      },
+      sismoConnectRequest: {
+        appId: "0x2301cda9ff54957f06fa9750ad537ef5",
+        authRequests: [{ authType: AuthType.VAULT }],
+        claimRequests: [{ groupId: "0xd4cdaae916464870f7413a03b1fe8c31" }],  // UPDATE HERE WITH YOUR GROUP
+      },
+      templateConfig: {
+        nftMetadata: {
+          name: "NFT Claim leo test",  // UPDATE HERE
+          description: "This is a leo airdrop test",// UPDATE HERE
+          image: "leo_cover_400x400.png" // UPDATE HERE
+        },
+        chains: [{
+          contractAddress: "{{ auto-fill }}",
+          name: "sepolia", // UPDATE HERE choose your chain name
+          relayerEnabled: false, 
+          isTransferable: true
+        }, {
+          contractAddress: "{{ auto-fill }}",
+          name: "goerli", // UPDATE HERE choose your chain name
+          relayerEnabled: false, 
+          isTransferable: true
+        }, {
+          contractAddress: "{{ auto-fill }}",
+          name: "mumbai", // UPDATE HERE choose your chain name
+          relayerEnabled: false, 
+          isTransferable: false
+        }, {
+          contractAddress: "{{ auto-fill }}",
+          name: "gnosis", // UPDATE HERE choose your chain name
+          relayerEnabled: false, 
+          isTransferable: false
+        }, {
+          contractAddress: "{{ auto-fill }}",
+          name: "polygon", // UPDATE HERE choose your chain name
+          relayerEnabled: false, 
           isTransferable: true
         }],
         step1CtaText: "Prove eligibility", 

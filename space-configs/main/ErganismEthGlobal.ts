c7c2d533ccc5e56468935321f685884d189f8d83
MrPinut <50791972+MrPinut@users.noreply.github.com>
2023-07-22 14:32:55 +0200

    Create ErganismEthGlobal.ts

---
+++ space-configs/main/ErganismEthGlobal.ts
@@ -0,0 +1,31 @@
+import { SpaceConfig } from "../types";
+
+//           UPDATE HERE ↓↓↓
+export default {
+  metadata: {
+    name: "ErganismEthGlobal", // UPDATE HERE
+    description:
+      "We are the Erganism Team, we are looking to use Sismo!", // UPDATE HERE
+    image: "erganismEthGlobale_picture_400x400.png", // UPDATE HERE
+    socialLinks: [
+      {
+        type: "github", 
+        link: "https://github.com/Erganic", // UPDATE HERE
+      },
+      // OTHER TYPE OF LINK YOU CAN USE
+      // {
+      //   type: "twitter",
+      //   link: "https://twitter.com/Sismo_eth",
+      // },
+      // {
+      //   type: "discord",
+      //   link: "https://discord.com/invite/sismo",
+      // },
+      // {
+      //   type: "github",
+      //   link: "https://github.com/Erganic",
+      // },
+    ],
+  },
+  apps: [],
+} as SpaceConfig;

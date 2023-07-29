7b33da5fb5313e578182044a27daaad864a8bdc9
midbutnotinthemid <140858664+midbutnotinthemid@users.noreply.github.com>
2023-07-29 16:15:05 +0200

    Update OP-Citizen-Sismo-Badge.ts
    made changes according to https://app-store-resources.sismo.io/sismo-app-store-tutorials/cheatsheet-zkdrop

--- space-configs/main/OP-Citizen-Sismo-Badge.ts
+++ space-configs/main/OP-Citizen-Sismo-Badge.ts
@@ -26,7 +26,7 @@ export default {
   apps: [
       {
       // NO UPDATE. Template type
-      type: "zkForm",
+      type: "zkDrop",
       // UPDATE. App information
       metadata: {
         name: "OP Citizen", // UPDATE. Your app name
@@ -40,41 +40,38 @@ export default {
       },
       // UPDATE. Sismo Connect request, see "Cheatsheet: Build Your Sismo Connect Request"
       sismoConnectRequest: {
-        appId: "0x18aefc6c64fd2be0f4cb4cc0d16c9792", // NO UPDATE. Sismo Connect appId is automatically created based on your app metadata
+        appId: "{{ auto-fill }}", // NO UPDATE. Sismo Connect appId is automatically created based on your app metadata
         authRequests: [{ authType: AuthType.VAULT }], // UPDATE OR REMOVE. Select auth
-        claimRequests: [{ groupId: "0x3a027cf4f8a0c300a3ba1592caa2cb08" }], // UPDATE OR REMOVE. Select group
+        claimRequests: [{ groupId: "0xc90878eaa974c31bc62c52ad86121765" }], // UPDATE OR REMOVE. Select group
       },
       // UPDATE. Config for zkForm
       templateConfig: {
+        owner: "{{ evm address }}", 
+        isTransferable: true,
+        nftMetadata: {
+          name: "OPimism citizen badge holder", // UPDATE. Name for your NFT
+          description: "Claimable by the early collective member of Optimism.", // UPDATE. Description for your NFT
+          image: "op-citizen", // UPDATE. Image for your NFT, to be placed in the images folder
+          symbol: "OPCitizenNFT", // UPDATE. Symbol for your NFT
+    },
+      chains: [
+      {
+        contractAddress: "{{ auto-fill }}", // The contract address will be automatically created based on your chain name
+        name: "optimism", // UPDATE. Your chain name: "gnosis", "mainnet", "arbitrum", "optimism", "polygon", “mumbai”, “arbitrum-goerli”, “goerli”, “optimism-goerli”
+        relayerEnabled: true, // UPDATE. Relayer enabled or not to pay fees for users and ensure full privacy-preserving claiming, set to false by default.
+      },
+    ],
         step1CtaText: "Enter the Sybil-Resistant Lottery", // UPDATE OR REMOVE. 1st step text when users click on the app, "Sign in with Sismo" by default
         step2CtaText: "Get your Optimism Citizen badge", // UPDATE. 2nd step button text, on submitting the form
         appDescription: "Prove that you have KYCed or selected citizen at Optimism Collective.", // UPDATE OR REMOVE. Description for your app
         // UPDATE OR REMOVE. Form fields
-        fields: [ 
-          {
-            type: "short-text", // NO UPDATE. Your field type
-            label: "Claiming Address", // UPDATE. Your field name
-            placeholder: "Provide the address you will use to claim the NFT.", // UPDATE OR REMOVE. Your helper text
-            isRequired: true, // UPDATE OR REMOVE. Field required or not, true by default
-          },
-        ],
+
         // UPDATE OR REMOVE. Success form message
         congratulationsMessage: {
           title: "Congrats!", // UPDATE. Your title
           description: "We are lucky to have you among us 💜 ", // UPDATE. Your message
         },
-        // UPDATE OR REMOVE. User selection
-        // userSelection: { type: "Lottery", maxNumberOfEntries: 10, numberOfWinners: 10 }, // UPDATE. Your type: "Lottery" or "FCFS". If "FCFS", only use parameter "maxNumberOfUsers". See Parameters explanation below.
-        // UPDATE. Data storage
-        output: {
-          destination: {
-            type: "google_sheet", // NO UPDATE.
-            spreadsheetId: "1Jl_pdYjOlGie8mqLVpRMpY3JDHVEPYKBaP9qOHCcoK0", // UPDATE. See "Add your App in the Sismo App Store"
-          },
-          // saveAuths: true, // UPDATE OR REMOVE. Save Auths outputs in spreadsheet
-          // saveClaims: true, // UPDATE OR REMOVE. Save Claims outputs in spreadsheet
-        },
-      },
+
       // UPDATE OR REMOVE. App dates
       options: {
         // startDate: new Date("2023-07-29T18:00"), // UPDATE OR REMOVE. Your start date YYYY-MM-DD

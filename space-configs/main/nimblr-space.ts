22f0835122fc3469f15da682731bc1827d69cc85
ahrevival <96093950+ahrevival@users.noreply.github.com>
2023-08-25 08:53:14 -0400

    Update nimblr-space.ts

--- space-configs/main/nimblr-space.ts
+++ space-configs/main/nimblr-space.ts
@@ -6,7 +6,7 @@ export default {
     name: "My Awesome Space", // UPDATE HERE
     description:
       "This Space is dedicated to my new project. It's a great project, you should check it out!", // UPDATE HERE
-    image: "my_awesome_space_picture_400x400.png", // UPDATE HERE
+    image: "Property 1=Variant5.png", // UPDATE HERE
     socialLinks: [
       {
         type: "link", 

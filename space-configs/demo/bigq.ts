f22da72543a64dbf44be65421aec9017d28b7cb1
bigq <yum0e@protonmail.com>
2023-09-11 18:08:10 +0200

    feat: test zkbot for zkform

--- space-configs/demo/bigq.ts
+++ space-configs/demo/bigq.ts
@@ -64,5 +64,65 @@ export default {
         appDescription: "Claim Your Tanjiro!",
       },
     },
+    {
+      type: "zkForm",
+      metadata: {
+        slug: "event",
+        name: "Cow Event Invitation",
+        description:
+          "Prove you are a Cow Trader or Holder to register to an invitation-only event organized by Cow Swap, during EthCC week in Paris.",
+        tags: ["Event", "Ticket"],
+        image: "cowswap_app_store_cow_event_400x400.png",
+        createdAt: new Date("2023-07-04T18:00"),
+      },
+      sismoConnectRequest: {
+        appId: "0x42bf819b60a7f4cbe57f2c5617b6a35c",
+        authRequests: [{ authType: AuthType.VAULT }],
+        claimRequests: [
+          {
+            groupId: "0x70c30e9a9abdb5fd41ba9e9cb7f50173",
+            value: 1,
+          },
+        ],
+        impersonateAddresses: [
+          "0xb18e3bf33365fd2466c2e99b181e527a165c210c",
+          "dhadrien.sismo.eth",
+          "0xA4C94A6091545e40fc9c3E0982AEc8942E282F38",
+          "0x1b9424ed517f7700e7368e34a9743295a225d889",
+          "0x82fbed074f62386ed43bb816f748e8817bf46ff7",
+          "0xc281bd4db5bf94f02a8525dca954db3895685700",
+          "telegram:dhadrien:1234",
+          "github:dhadrien",
+          "twitter:dhadrien_:2390703980",
+        ],
+      },
+      templateConfig: {
+        step2CtaText: "Register to get your Ticket",
+        fields: [
+          {
+            type: "short-text",
+            label: "Email",
+            isRequired: true,
+          },
+          {
+            type: "short-text",
+            label: "Name or Pseudonym",
+            isRequired: true,
+          },
+        ],
+        congratulationsMessage: {
+          title: "Congratulations!",
+          description:
+            "Tickets are processed on a first come first serve basis. You will receive an email update in a few days! See you there 💜",
+        },
+        output: {
+          destination: {
+            type: "google_sheet",
+            spreadsheetId: "1oENmskjo374TPD5dWikjVN5W610YUwIp22WvxrDBYd8",
+          },
+          saveAuths: true,
+        },
+      },
+    },
   ],
 } as SpaceConfig;

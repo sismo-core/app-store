03c48e6208d287f7f214cc87eabefb09a934af51
robsven <140515327+robsven@users.noreply.github.com>
2023-09-21 09:45:43 +0200

    Create Robs-cave.ts

---
+++ space-configs/main/Robs-cave.ts
@@ -0,0 +1,22 @@
+import { SpaceConfig } from "../types";
+
+//           UPDATE HERE ↓↓↓
+export default {
+  metadata: {
+    name: "Robs cave", // UPDATE HERE
+    description:
+      "Welcome to my cave, don't expect much but feel free to dig around.", // UPDATE HERE
+    image: "Abel_E_bunny_400x400.png", // UPDATE HERE
+    socialLinks: [
+      {
+        type: "twitter", 
+        link: "https://twitter.com/robsvensek", // UPDATE HERE
+      },
+            {
+        type: "github", 
+        link: "https://github.com/robsven", // UPDATE HERE
+      },
+    ],
+  },
+  apps: [],
+} as SpaceConfig;
